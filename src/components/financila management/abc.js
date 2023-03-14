fadb741d55e0915964164fcdeb30e7d6b26be34c
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-03-14 22:17:22 +0530

    123

---
+++ src/components/financila management/abc.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Abc extends Component {
+    render() {
+        return (
+            <div>
+                <h3>abcdefghijklmnop</h3>
+                <h2>jbbjbj </h2>
+                <h5>g gh  b bjhh  hbjb </h5>
+            </div>
+        );
+    }
+}
+
+export default Abc;
