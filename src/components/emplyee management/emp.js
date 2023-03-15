11821a224b1ee3caf1e56f34af5564e0459ee2ef
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-03-15 23:21:52 +0530

    employee component added

---
+++ src/components/emplyee management/emp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Emp extends Component {
+    render() {
+        return (
+            <div>
+                <h1>this is first test</h1>
+            </div>
+        );
+    }
+}
+
+export default Emp;
