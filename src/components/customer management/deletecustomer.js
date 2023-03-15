148cc289017f311ace14aac6550a0129ea29b0d2
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-03-16 00:15:06 +0530

    again modifed delete file

--- src/components/customer management/deletecustomer.js
+++ src/components/customer management/deletecustomer.js
@@ -1,1 +1,13 @@
-change1
+import React, { Component } from 'react';
+
+class Deletecustomer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>form</h2>
+            </div>
+        );
+    }
+}
+
+export default Deletecustomer;
