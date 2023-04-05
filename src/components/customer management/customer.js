550bf3c47dc3b53ced566a37a3fe0aedf09cfa29
sathmijayasuriya <sathmijayasuriya@gmail.com>
2023-04-05 19:24:44 +0530

    add part

--- src/components/customer management/customer.js
+++ src/components/customer management/customer.js
@@ -5,6 +5,7 @@ class Customer extends Component {
         return (
             <div>
                 <h1>this is customer profile</h1>
+                <h2>customer name;</h2>
             </div>
         );
     }
